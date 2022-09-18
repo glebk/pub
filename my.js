705a06f6b7637715ad51fb0466c3881d2221ce98
Gleb Keselman <gleb.keselman@gmail.com>
2022-09-18 18:56:31 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,1 +1,12 @@
-var daten = Date.now();
+var sdate = Date.now();
+function update_time() {
+  var edate = Date.now();
+  var ss = Math.floor((edate - sdate) / 1000);
+  var mm = Math.floor(ss / 60);
+  var hh = Math.floor(mm / 60);
+  ss = ss % 60;
+  mm = mm % 60;
+  hh = hh % 24;
+  document.getElementById("text-0000000f").value = hh + ":" + mm + ":" + ss;
+}
+setInterval(update_time,1000);
